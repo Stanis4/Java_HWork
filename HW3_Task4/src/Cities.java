97b0bf1a900bf1fa3812014a97cf68b81898cfdb
Stanis4 <49916856+Stanis4@users.noreply.github.com>
2019-05-23 18:09:44 +0300

    Update Cities.java

--- HW3_Task4/src/Cities.java
+++ HW3_Task4/src/Cities.java
@@ -1,37 +1,38 @@
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Cities {
     public static void main(String[] args) {
         String[] city = new String[]{"Москва", "Амстердам", "Минск", "Курск", "Киев", "Львов", "Винница"};
-        int array_capacity = city.length;
+        String[] used_city = city;
+
 
         //вывод приветствия
         System.out.println("Давай поиграем в ‘Города’. Ты первый называй!»");
 
-        for (int i=0; i<array_capacity; i++){
 
             //ввод данных
-            Scanner sc = new Scanner(System.in);
-            String input_variable = sc.nextLine();
-            String input_city = input_variable.toUpperCase();
+        System.out.print("Твоё слово - ");
+        Scanner sc = new Scanner(System.in);
+        String input_variable = sc.nextLine();
+        String input_city = input_variable.toUpperCase();
 
-            //проверка последней буквы слова
-            int last = input_city.length()-1;
-            char ch = input_city.charAt(last);
+        //проверка последней буквы слова
+        int last = input_city.length() - 1;
+        Character ch = input_city.charAt(last);
 
-            if (ch==city[1][i]
+        System.out.println("Последняя буква "+ ch);
+        String str = ch.toString();
 
-        }
-            System.out.println("Iteration "+i);
+            for (int i = 0; i<used_city.length;i++) {
 
-
-
-
-
-
-        //запоминаем уже взятые из массива слова
-
-
+             if (used_city[0].charAt(1)==ch) {
+                System.out.println("Моё слово - " + city[i]);
+                break; }
+             /*else if (){
+                System.out.println("Я проиграл, ты знаешь больше городов :)");
+                }*/
+            }
 
     }
 }
